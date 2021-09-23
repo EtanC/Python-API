0115c705d9fe11ffb75256d60facbd54326bcf4b
Braian <z5358768@ad.unsw.edu.au>
2021-09-23 17:50:41 +1000

    Added black box tests for the full implementation of auth_register and auth_login

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -4,15 +4,39 @@ from src.auth import auth_register_v1, auth_login_v1
 from src.other import clear_v1
 from src.error import InputError
 
+## TODO
+# - handle_str (auth_register_v1)
+
 @pytest.fixture
 def reset_data():
     clear_v1()
+
+# Black box test valid register and login (testing auth.py)
+
+def test_valid(reset_data):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    result = auth_register_v1(email, password, name_first, name_last)
+    assert auth_login_v1(email, password) == result
+
+# Tests for error checking auth_register
 
 def test_invalid_email(reset_data):
     email = "uhh, im also a real email?"
     password = "asdfghjkl"
     name_first = "Bill"
     name_last = "Thompson"
+    with pytest.raises(InputError):
+        auth_register_v1(email, password, name_first, name_last)
+
+def test_email_repeat(reset_data):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
     with pytest.raises(InputError):
         auth_register_v1(email, password, name_first, name_last)
 
@@ -39,3 +63,27 @@ def test_long_lastname(reset_data):
     name_last = "Smith" * 20
     with pytest.raises(InputError):
         auth_register_v1(email, password, name_first, name_last)
+
+# Test for error checking auth_login
+
+def test_wrong_password(reset_data):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
+    
+    password = "wrong_password"
+    with pytest.raises(InputError):
+        auth_login_v1(email, password)
+
+def test_wrong_email(reset_data):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
+    
+    email = "wrong_email@outlook.com"
+    with pytest.raises(InputError):
+        auth_login_v1(email, password)
