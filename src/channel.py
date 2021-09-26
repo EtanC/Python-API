f37e1920a69334135bdcb4a5935f87732c937f0c
Braian <z5358768@ad.unsw.edu.au>
2021-09-26 19:43:20 +1000

    Added docstrings, fixed a typo

--- src/channel.py
+++ src/channel.py
@@ -29,6 +29,10 @@ def channel_details_v1(auth_user_id, channel_id):
     }
 
 def get_channel(channel_id):
+    '''
+    Searches for a channel in the data_store with the given channel_id
+    Returns None if the channel was not found
+    '''
     store = data_store.get()
     for channel in store['channels']:
         if channel['channel_id'] == channel_id:
@@ -36,6 +40,9 @@ def get_channel(channel_id):
     return None
 
 def is_channel_member(auth_user_id, members):
+    '''
+    Verifies if the auth_user_id is in the list of members for a channel
+    '''
     for user in members:
         if user['u_id'] == auth_user_id:
             return True
@@ -43,6 +50,15 @@ def is_channel_member(auth_user_id, members):
             
 
 def channel_messages_v1(auth_user_id, channel_id, start):
+    '''
+    Returns up to 50 messages from the specified channel given a starting index
+    Also returns the starting and ending indexes of the returned messages
+    Will raise InputError if
+        - channel_id is invalid
+        - start is greater than the total number of messages
+    Will raise AccessError if
+        - auth_user_id is not a member of the channel
+    '''
     # Checking channel_id is valid
     channel = get_channel(channel_id)
     if channel == None:
@@ -52,7 +68,7 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         raise InputError("Invalid start")
     # Checking auth_user_id is part of channel
     if is_channel_member(auth_user_id, channel['members']):
-        raise AccessError("User is not a member of the channnel")
+        raise AccessError("User is not a member of the channel")
 
     return {
         'messages': [
