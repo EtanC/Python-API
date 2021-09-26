ff315cebd3ee0684309f47a46474a0f10b68cb49
Braian <z5358768@ad.unsw.edu.au>
2021-09-26 19:36:41 +1000

    Completed error checking for channel_messages_v1

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+from src.data_store import data_store
+from src.error import InputError, AccessError
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
@@ -25,7 +28,32 @@ def channel_details_v1(auth_user_id, channel_id):
         ],
     }
 
+def get_channel(channel_id):
+    store = data_store.get()
+    for channel in store['channels']:
+        if channel['channel_id'] == channel_id:
+            return channel
+    return None
+
+def is_channel_member(auth_user_id, members):
+    for user in members:
+        if user['u_id'] == auth_user_id:
+            return True
+    return False
+            
+
 def channel_messages_v1(auth_user_id, channel_id, start):
+    # Checking channel_id is valid
+    channel = get_channel(channel_id)
+    if channel == None:
+        raise InputError("Invalid channel")
+    # Checking start is valid
+    if start > len(channel['messages']):
+        raise InputError("Invalid start")
+    # Checking auth_user_id is part of channel
+    if is_channel_member(auth_user_id, channel['members']):
+        raise AccessError("User is not a member of the channnel")
+
     return {
         'messages': [
             {
