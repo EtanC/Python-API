3bb6e2326d037997c947c3aa8d74da252d369e0d
z5360742 <z5360742@cse.unsw.edu.au>
2021-09-26 21:52:54 +1000

    Implemented code for channels_create_v1

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from src.data_store import data_store
+from src.error import InputError 
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +22,29 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+    # Checking for length of channel name 
+    if (len(name) < 1) or (len(name) > 20): 
+        raise InputError("Channel name must be between 1 and 20 characters long")
+
+
+    store = data_store.get()
+
+    channel_id = len(store['channels']) + 1 
+
+    # Store channel data in a dictionary 
+    channel_data = { 
+        'channel_id': channel_id,
+        'name': name,
+        'is_public': is_public,
+        'owner_members': [auth_user_id],
+        'all_members': [auth_user_id], 
+
+    }
+    
+    # Append channel_data to 'channels' list in data_store 
+    store['channels'].append(channel_data)
+
+
     return {
-        'channel_id': 1,
+        'channel_id': channel_id,
     }
