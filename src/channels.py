c7e6b568ec24cb1a81ba27aa983857fd6942da5b
z5364292 <z5364292@cse.unsw.edu.au>
2021-09-29 18:34:24 +1000

    unfinished channels.py

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,18 @@
+'''
+{channels_list_v1}
+
+No input error -> assuming that auth_user_id is always valid. 
+Provide a list of all channels (and their associated details) 
+that the authorised user is part of.
+
+'''
+
+from src.data_store import data_store
+from src.error import InputError 
+from src.error import AccessError 
+
+# if it is valid it shouldnt raise an error 
+# 
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -8,6 +23,13 @@ def channels_list_v1(auth_user_id):
         ],
     }
 
+'''
+
+{channels_listall_v1}
+
+Provide a list of all channels, including private channels, (and their associated details)
+
+'''
 def channels_listall_v1(auth_user_id):
     return {
         'channels': [
