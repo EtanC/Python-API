2961e1150a632a055888d1607af48bbebcb1e8e8
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-01 16:31:41 +1000

    much improved with helper functions

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 from src.data_store import data_store
 from src.error import InputError
 from src.error import AccessError
+from src.channels import channels_list_v1
 
 import re
 
@@ -11,6 +12,7 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 def channel_details_v1(auth_user_id, channel_id):
     return {
         'name': 'Hayden',
+
         'owner_members': [
             {
                 'u_id': 1,
@@ -20,6 +22,7 @@ def channel_details_v1(auth_user_id, channel_id):
                 'handle_str': 'haydenjacobs',
             }
         ],
+
         'all_members': [
             {
                 'u_id': 1,
@@ -27,6 +30,12 @@ def channel_details_v1(auth_user_id, channel_id):
                 'name_first': 'Hayden',
                 'name_last': 'Jacobs',
                 'handle_str': 'haydenjacobs',
+            }
+
+            {
+
+
+
             }
         ],
     }
@@ -45,38 +54,60 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         'end': 50,
     }
 
-def channel_join_v1(auth_user_id, channel_id):
-    '''
-    YOU SHOULD BE ABLE TO READ ALL THIS INSIDE "channel_join_v1_BenH" branch
-    '''
 
+#helper function to find if a channel exists based on the id
+def valid_channel_id(channel_id):
+
+    store = data_store.get()
+
+    for channel in store['channels']:
+        if channel['channel_id'] == channel_id:
+            return True
+    return False
+
+#helper function to find if a channel exists based on the id
+def find_user_in_channel(auth_user_id, channel_id):
+
+    store = data_store.get()
+    channels_list = channels_list_v1(auth_user_id):
+        
+    for channel in channels_list:
+        if channels_list[channel]['channel_id'] == channel_id:
+            return True
+    
+    return False
+
+
+
+def channel_join_v1(auth_user_id, channel_id):
+
+    '''
     #Given a channel_id of a channel that the authorised user can join, 
     #adds them to that channel.
     #auth_user_id and channel_id are in integer form
+    '''
+
     store = data_store.get() # get the data
+    channel = channel_details_v1(auth_user_id, channel_id)
 
-
-    #channel_id does not refer to a valid channel
-    for channel in store['channels']:
-        if channel['channel_id'] == channel_id:
-            break
+    #check if channel is valid
+    if valid_channel_id(channel_id) == False:
         raise InputError("channel_id is INVALID")
 
-    #the authorised user is already a member of the channel
-    if ...:
-        raise InputError("User already in channel")
-
-    #channel_id refers to a channel that is private 
-    #and the authorised user is not already a channel member 
-    #and is not a global owner
-
-    for channel in store['channels']:
-        if channel['Private'] == True:
-            raise AccessError("This channel is PRIVATE")
-            break
-        else:
-            channel['members'] = auth_user_id
-
+    elif valid_channel_id(channel_id) == True:
         
+        #check if the user is already part of the channel
+        if find_user_in_channel(auth_user_id, channel_id) == True:
+            raise InputError("User already in channel")    
+
+        #check if the chanel is private and whether the user if part 
+        #of the channel or not
+        else:
+            if channel['is_public'] == False:
+            raise AccessError("This channel is PRIVATE")
+
+    #add the user to the channel
+    channel['all_members'][auth_user_id] = auth_user_id
     return {
     }
+
