005e7f0ac2c04c1f8a85b883e1120a01b03016bd
James Mead <z5358774@ad.unsw.edu.au>
2021-10-01 20:59:01 +1000

    Completed tests, making progress on implementation

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,46 @@
+from src.data_store import data_store
+from src.error import InputError, AccessError
+from src.channel import check_valid_user_id
+
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
-    return {
-    }
+
+    store = data_store.get()
+
+    # check for invalid user id 
+    if check_valid_user_id(auth_user_id, store) == False: 
+        raise AccessError("Invalid auth_user_id")
+    
+    # check for invalid channel id 
+    if check_valid_channel(channel_id, store) == False: 
+        raise InputError("Invalid channel id")
+
+    # check whether member is in the channel or not 
+    if check_member_in_channel(auth_user_id, channel_id, store) == True: 
+        raise InputError("Authorised user is already a member of the channel")
+
+
+
+def valid_user_id(u_id):
+    store = data_store.get()
+
+    for user in store['users']:
+        if user['user_id'] == u_id:
+            return True
+    return False
+
+def valid_channel_id(channel_id):
+    store = data_store.get()
+
+    for channel in store['channels']:
+        if channel['channel_id'] == channel_id:
+            return True
+    return False
+
+def not_in_channel(channel_id, u_id):
+
+def not_authorised_user(auth_user_id):
+
 
 def channel_details_v1(auth_user_id, channel_id):
     return {
@@ -42,3 +82,39 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
+
+def check_valid_channel(channel_id, store): 
+    result = False 
+
+    # if channel_id exists return True, else return False 
+    for channel in store['channels']: 
+        if channel_id == channel['channel_id']: 
+            result = True
+    return result 
+
+def check_member_in_channel(auth_user_id, channel_id, store): 
+    # put user info dictionaru into user_data 
+    user_data = {} 
+    for user in store['users']: 
+        if auth_user_id == user['u_id']: 
+            user_data = user
+            break
+
+    # put channel data into channel_data
+    channel_data = {}
+    for channel in store['channels']: 
+        if channel_id == channel['channel_id']: 
+            channel_data = channel 
+            break
+    
+    # if user in members section of channel, return True else return False
+    if user_data in channel_data['all_members']: 
+        return True
+    else: 
+        return False 
+
+def find_channel(channel_id, store): 
+
+    for channel in store['channels']: 
+        if channel_id == channel['channel_id']: 
+            return channel 
--- tests/test_channel_invite.py
+++ tests/test_channel_invite.py
@@ -11,6 +11,8 @@ from src.error import AccessError
 def reset():
     clear_v1()
 
+def test_valid(reset):
+
     email = "realemail_812@outlook.edu.au"
     password = "Password1"
     name_first = "John"
@@ -27,58 +29,146 @@ def reset():
     auth_register_v1(email, password, name_first, name_last)
     result = auth_login_v1(email, password) 
     # take auth user id from returned dictionary 
-    # auth_user_id_2 = result['auth_user_id']
-    return auth_user_id
-
-def test_valid(reset):
-    auth_user_id = reset
+    auth_user_id_2 = result['auth_user_id']
 
     channel_name = "channel1_"
     is_public = True
     result = channels_create_v1(auth_user_id, channel_name, is_public)
     channel_id = result['channel_id']
-
-    # u_id of the second user that was created
-    u_id = "2"
     
-    channel_invite_v1(auth_user_id, channel_id, u_id)
-    # assert that user is now in channel
+    channel_invite_v1(auth_user_id, channel_id, auth_user_id_2)
+
+    channel_data = channel_details_v1(passes in)
+    members_list = channel_data['all_members']
+
+    member_in_channel = False
+    for members in members_list:
+        if members['u_id'] == auth_user_id_2:
+            member_in_channel = True
+
+    assert member_in_channel == True
+
     
 def test_invalid_channel(reset):
-    auth_user_id = reset
 
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id = result['auth_user_id']
+
+    email = "fakeemail_812@outlook.edu.au"
+    password = "Password2"
+    name_first = "Chris"
+    name_last = "Zell"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id_2 = result['auth_user_id']
+    
     channel_name = "channel1_"
     is_public = True
     result = channels_create_v1(auth_user_id, channel_name, is_public)
     # Changing the channel id
     channel_id = result['channel_id'] * 20
-
-    # u_id of the second user that was created
-    u_id = "2"
     
     with pytest.raises(InputError):
-        channel_invite_v1(auth_user_id, channel_id, u_id)
+        channel_invite_v1(auth_user_id, channel_id, auth_user_id_2)
 
 def test_invalid_u_id(reset):
-    auth_user_id = reset
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id = result['auth_user_id']
 
+    email = "fakeemail_812@outlook.edu.au"
+    password = "Password2"
+    name_first = "Chris"
+    name_last = "Zell"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id_2 = result['auth_user_id']
+    
     channel_name = "channel1_"
     is_public = True
     result = channels_create_v1(auth_user_id, channel_name, is_public)
     # Changing the channel id
     channel_id = result['channel_id']
 
-    # u_id increments by 1 for every new user, but there are only two
-    # so this is an invalid u_id
-    u_id = "10"
+    auth_user_id_2 = auth_user_id_2 * 20
     
     with pytest.raises(InputError):
-        channel_invite_v1(auth_user_id, channel_id, u_id)
+        channel_invite_v1(auth_user_id, channel_id, auth_user_id_2)
 
 def test_already_in_channel(reset):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id = result['auth_user_id']
 
+    email = "fakeemail_812@outlook.edu.au"
+    password = "Password2"
+    name_first = "Chris"
+    name_last = "Zell"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id_2 = result['auth_user_id']
+    
+    channel_name = "channel1_"
+    is_public = True
+    result = channels_create_v1(auth_user_id, channel_name, is_public)
+    # Changing the channel id
+    channel_id = result['channel_id']
+
+    auth_user_id_2 = auth_user_id_2
+
+    channel_invite_v1(auth_user_id, channel_id, auth_user_id_2)
+    
+    with pytest.raises(InputError):
+        channel_invite_v1(auth_user_id, channel_id, auth_user_id_2)
 
 def test_not_member(reset):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id = result['auth_user_id']
 
+    email = "fakeemail_812@outlook.edu.au"
+    password = "Password2"
+    name_first = "Chris"
+    name_last = "Zell"
+    auth_register_v1(email, password, name_first, name_last)
+    result = auth_login_v1(email, password) 
+    # take auth user id from returned dictionary 
+    auth_user_id_2 = result['auth_user_id']
+    
+    channel_name = "channel1_"
+    is_public = True
+    result = channels_create_v1(auth_user_id, channel_name, is_public)
+    # Changing the channel id
+    channel_id = result['channel_id']
+
+    auth_user_id = auth_user_id * 20
+    
+    with pytest.raises(AccessError):
+        channel_invite_v1(auth_user_id, channel_id, auth_user_id_2)
+      
 
 
