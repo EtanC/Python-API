64615a063b89107882e2fb84997df98e6f8b56f9
James Mead <z5358774@ad.unsw.edu.au>
2021-10-02 19:41:19 +1000

    Passed all tests with implementation

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,8 @@
-from src.data_store import data_store
-from src.error import InputError, AccessError
-from src.channels import check_valid_user_id
+from src.channels import check_valid_user_id 
+from src.data_store import data_store 
+from src.error import InputError, AccessError 
 
+from copy import deepcopy
 
 def channel_invite_v1(auth_user_id, channel_id, u_id):
 
@@ -39,31 +40,91 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
 
     return {}
 
-
 def channel_details_v1(auth_user_id, channel_id):
+
+    store = data_store.get() 
+
+    # check for invalid user id 
+    if check_valid_user_id(auth_user_id, store) == False: 
+        raise AccessError("Invalid auth_user_id")
+    
+    # check for invalid channel id 
+    if check_valid_channel(channel_id, store) == False: 
+        raise InputError("Invalid channel id")
+
+    # check whether member is in the channel or not 
+    if check_member_in_channel(auth_user_id, channel_id, store) == False: 
+        raise AccessError("Authorised user is not a member of the channel")
+
+    # copying member and owner list into temporary lists 
+    channel = find_channel(channel_id, store) 
+    #mem_list = channel['all_members'].copy() 
+    #own_list = channel['owner_members'].copy() 
+
+    # run deepcopy to keep original data store untouched 
+    mem_list = deepcopy(channel['all_members'])
+    own_list = deepcopy(channel['owner_members'])
+    
+
+    # go through list of members in temp list and remove their password info 
+    for member in mem_list: 
+        del member['password'] 
+    
+    # go through list of owners in temp list and remove their password info 
+    for owner in own_list: 
+        del owner['password']
+    
+
+
     return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
+        'name': channel['name'],
+        'is_public': channel['is_public'], 
+        'owner_members': own_list,
+        'all_members': mem_list,
     }
 
+def get_channel(channel_id):
+    '''
+    Searches for a channel in the data_store with the given channel_id
+    Returns None if the channel was not found
+    '''
+    store = data_store.get()
+    for channel in store['channels']:
+        if channel['channel_id'] == channel_id:
+            return channel
+    return None
+
+def is_channel_member(auth_user_id, members):
+    '''
+    Verifies if the auth_user_id is in the list of members for a channel
+    '''
+    for user in members:
+        if user['u_id'] == auth_user_id:
+            return True
+    return False
+            
+
 def channel_messages_v1(auth_user_id, channel_id, start):
+    '''
+    Returns up to 50 messages from the specified channel given a starting index
+    Also returns the starting and ending indexes of the returned messages
+    Will raise InputError if
+        - channel_id is invalid
+        - start is greater than the total number of messages
+    Will raise AccessError if
+        - auth_user_id is not a member of the channel
+    '''
+    # Checking channel_id is valid
+    channel = get_channel(channel_id)
+    if channel == None:
+        raise InputError("Invalid channel")
+    # Checking start is valid
+    if start > len(channel['messages']):
+        raise InputError("Invalid start")
+    # Checking auth_user_id is part of channel
+    if not is_channel_member(auth_user_id, channel['all_members']):
+        raise AccessError("User is not a member of the channel")
+
     return {
         'messages': [
             {
@@ -91,7 +152,8 @@ def check_valid_channel(channel_id, store):
     return result 
 
 def check_member_in_channel(auth_user_id, channel_id, store): 
-    # put user info dictionaru into user_data 
+    # put user info dictionary into user_data 
+
     user_data = {} 
     for user in store['users']: 
         if auth_user_id == user['u_id']: 
@@ -116,3 +178,5 @@ def find_channel(channel_id, store):
     for channel in store['channels']: 
         if channel_id == channel['channel_id']: 
             return channel 
+    return None 
+
