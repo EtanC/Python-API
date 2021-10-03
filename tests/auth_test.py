b590b54875c9211eb198f2b3c91614bbb59133bf
Braian <z5358768@ad.unsw.edu.au>
2021-10-03 20:39:28 +1100

    Added test for handle string function in register

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,6 +1,8 @@
 import pytest
 
 from src.auth import auth_register_v1, auth_login_v1
+from src.channel import channel_details_v1
+from src.channels import channels_create_v1
 from src.other import clear_v1
 from src.error import InputError
 
@@ -44,6 +46,39 @@ def test_valid_multiple_register(reset_data):
     assert auth_login_v1(email1, password1) == result1
     assert auth_login_v1(email2, password2) == result2
     assert auth_login_v1(email3, password3) == result3
+
+# Testing handle_str
+
+def test_handle_str(reset_data):
+    email = "realemail_812@outlook.edu.au"
+    password = "Password1"
+    name_first = "John"
+    name_last = "Smith"
+    user_id = auth_register_v1(email, password, name_first, name_last)['auth_user_id']
+    channel_id = channels_create_v1(user_id, "channel_name", is_public=True)['channel_id']
+    expected = {
+        'name' : "channel_name",
+        'is_public' : True,
+        'owner_members' : [
+            {
+                'u_id' : user_id,
+                'email' : "realemail_812@outlook.edu.au",
+                'name_first' : "John",
+                'name_last' : "Smith",
+                'handle' : "johnsmith"
+            },
+        ],
+        'all_members' : [
+            {
+                'u_id' : user_id,
+                'email' : "realemail_812@outlook.edu.au",
+                'name_first' : "John",
+                'name_last' : "Smith",
+                'handle' : "johnsmith"
+            },
+        ],
+    }
+    assert channel_details_v1(user_id, channel_id) == expected
 
 # Tests for error checking auth_register
 
