26afddd6905fd5cb379e77372a3bbe958c07c495
cs1531 <cs1531@cse.unsw.edu.au>
2021-10-05 23:34:00 +1100

    Results push

--- assumptions.md
+++
@@ -1,2 +0,0 @@
-channels_create_v1 assumptions: 
-* channel name can contain any char as long as it is within character range 
--- src/other.py
+++
@@ -1,9 +0,0 @@
-from src.data_store import data_store
-
-def clear_v1():
-    store = data_store.get()
-    store['users'] = []
-    store['channels'] = []
-    store['handle_append_no'] = {}
-    store['user_id_number'] = 1
-    data_store.set(store)
