6edaf209f58f2a04956822003cb3580a9a697710
Braian <z5358768@ad.unsw.edu.au>
2021-10-12 12:33:55 +1100

    Implemented routes in server.py to pass tests

--- src/server.py
+++ src/server.py
@@ -3,13 +3,14 @@ import signal
 from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
-from src.error import InputError
+from src.error import InputError, AccessError 
 from src.auth import auth_login_v1, auth_register_v1
 from src.other import clear_v1
 from src import config
-from src.message import message_send_v1
+from src.user import users_all_v1, user_profile_v1
 from src.channels import channels_create_v1
-from src.channel import channel_messages_v1
+from src.channel import channel_details_v1, channel_messages_v1
+from src.message import message_send_v1
 
 
 def quit_gracefully(*args):
@@ -45,19 +46,21 @@ def echo():
         'data': data
     })
 
-#AUTH
 @APP.route("/auth/login/v2", methods=['POST'])
 def auth_login_v2():
     '''
     Logs the user in after authenticating their identity
+
     Arguments:
         email    (str)      - The email belonging to the user logging in
         password (str)      - The password corresponding to the email
+
     Exceptions:
         InputError  - Occurs when email does not belong to a user
                     - Occurs when password is not correct
+
     Return Value:
-        Returns {'auth_user_id': user_id} on successful login
+        Returns {'token' : token, 'auth_user_id': user_id} on successful call
     '''
     data = request.get_json()
     user_id = auth_login_v1(data['email'], data['password'])
@@ -82,7 +85,7 @@ def auth_register_v2():
                     - Length of name_last is not between 1 and 50
 
     Return Value:
-        Returns {'auth_user_id': user_id} on successful register
+        Returns {'token' : token, 'auth_user_id': user_id} on successful call
     '''
     data = request.get_json()
     user_id = auth_register_v1(
@@ -99,26 +102,26 @@ def channel_messages():
     '''
     Returns up to 50 messages from (start), given a channel_id and token
 
-        Arguments:
-            token       (str)      - The token used to verify the user's identity
-            channel_id  (int)      - The channel's id, used to identify channel
-            start       (int)      - The number of the first message to return
-                                     eg. The most recent message would be 0
-                                         The second most recent message would be 1
-                                         And so on
+    Arguments:
+        token       (str)      - The token used to verify the user's identity
+        channel_id  (int)      - The channel's id, used to identify channel
+        start       (int)      - The number of the first message to return
+                                 eg. The most recent message would be 0
+                                     The second most recent message would be 1
+                                     And so on
 
-        Exceptions:
-            InputError  - Occurs when channel_id does not refer to a valid channel
-                        - Start is greater than number of messages in the channel
-            AccessError - channel_id is valid and authorised user is not a member
-                          of the channel
-                        - user_id does not refer to a valid user
+    Exceptions:
+        InputError  - Occurs when channel_id does not refer to a valid channel
+                    - Start is greater than number of messages in the channel
+        AccessError - channel_id is valid and authorised user is not a member
+                      of the channel
+                    - user_id does not refer to a valid user
 
 
-        Return Value:
-            Returns
-            {'messages' : messages, 'start' : start, 'end': end}
-            on successful call
+    Return Value:
+        Returns
+        {'messages' : messages, 'start' : start, 'end': end}
+        on successful call
     '''
     data = request.get_json()
     messages = channel_messages_v1(
@@ -153,15 +156,15 @@ def channels_create_v2():
     Creator of channel is immediately added to the channel. 
 
     Arguments:
-        token (str): token identifying user 
-        name (str): name of channel 
-        is_public (bool): whether channel is public (True) or private (False)
+        token       (str)     - token identifying user 
+        name        (str)     - name of channel 
+        is_public   (bool)    - whether channel is public (True) or private (False)
     
     Exceptions: 
         InputError  - Channel name not between 1 and 20 characters 
         AccessError - User not authorised 
 
-    Returns: 
+    Return Value: 
         Returns {channel_id} on successful creation 
     '''
 
@@ -170,6 +173,74 @@ def channels_create_v2():
     channel_id = channels_create_v1(data['token'], data['name'], data['is_public'])
 
     return dumps(channel_id)
+
+@APP.route("/channel/details/v2", methods=['GET'])
+def channel_details_v2(): 
+    '''
+    Given a channel with ID channel_id that the authorised user is a member of, 
+    provide basic details about the channel 
+
+    Arguments:
+        token       (str) - token identifying user
+        channel_id  (int) - id of channel 
+        
+    Exceptions: 
+        InputError  - Channel_id not valid 
+        AccessError - Authorised user not member of existing channel 
+                    - User not authorised 
+
+    Return Value: 
+        Returns {name, is_public, owner_members, all_members} on successful creation 
+    '''
+
+    data = request.get_json() 
+
+    return_dict = channel_details_v1(data['token'], data['channel_id'])
+    return dumps(return_dict) 
+
+@APP.route("/users/all/v1", methods=['GET'])
+def users_all(): 
+    '''
+    Given a user's token, return a list of all users and their associated details, 
+    including: u_id, email, name_first, name_last, handle_str
+    
+    Arguments:
+        token (str) - Token identifying user
+        
+    Exceptions: 
+        AccessError - User not authorised 
+
+    Return Value: 
+        Returns { users } on successful call  
+    ''' 
+    data = request.get_json() 
+
+    users = users_all_v1(data['token'])
+    return dumps({'users': users}) 
+
+@APP.route("/user/profile/v1", methods=['GET'])
+def user_profile(): 
+    '''
+    For a valid user, returns information about their u_id, email, first name, 
+    last name and handle_str.
+
+    Arguments: 
+        token   (str) - token idenfifying user1 (accessing the route) 
+        u_id    (int) - user id of the target / user2
+    
+    Exceptions: 
+        InputError  - u_id does not refer to a valid user2 
+        AccessError - user1 not authorised 
+    
+    Return Value: 
+        Returns { user } on successfull call
+    '''
+
+    data = request.get_json() 
+
+    user = user_profile_v1(data['token'], data['u_id'])
+
+    return dumps({'user': user})
 
 #MESSAGE
 @APP.route("/message/send/v1", methods=['POST'])
