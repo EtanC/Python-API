39368108f6aee7f47dd4c741c2fc71d8f52c78a4
Braian <z5358768@ad.unsw.edu.au>
2021-10-15 22:15:28 +1100

    Moved auth tests back to /tests to replace iter 1 tests as they are no longer needed

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,148 +1,270 @@
 import pytest
-
-from src.auth import auth_register_v1, auth_login_v1
-from src.channel import channel_details_v1
-from src.channels import channels_create_v1
-from src.other import clear_v1
-from src.error import InputError
-
-## TODO
-# - handle_str (auth_register_v1)
-
-@pytest.fixture
-def reset_data():
-    clear_v1()
+import requests
+from src import config
 
 # Black box test valid register and login (testing auth.py)
 
+@pytest.fixture
+def reset_data():
+    requests.delete(f"{config.url}clear/v1")
+
 def test_valid(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    result = auth_register_v1(email, password, name_first, name_last)
-    assert auth_login_v1(email, password) == result
-
-def test_valid_multiple_register(reset_data):
-    # Details for person 1
-    email1 = "realemail_812@outlook.edu.au"
-    password1 = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    result1 = auth_register_v1(email1, password1, name_first, name_last)
-    # Details for person 2
-    email2 = "realemail_318@outlook.edu.au"
-    password2 = "Password1"
-    name_first = "Bob"
-    name_last = "Johnson"
-    result2 = auth_register_v1(email2, password2, name_first, name_last)
-    # Details for person 3
-    email3 = "realemail_172@outlook.edu.au"
-    password3 = "Password1"
-    name_first = "Bill"
-    name_last = "Smith"
-    result3 = auth_register_v1(email3, password3, name_first, name_last)
-
-    assert auth_login_v1(email1, password1) == result1
-    assert auth_login_v1(email2, password2) == result2
-    assert auth_login_v1(email3, password3) == result3
+    data_register = {
+        "email" : "realemail_812@outlook.edu.au",
+        "password" : "Password1",
+        "name_first" : "John",
+        "name_last" : "Smith",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    data_login = {
+        "email" : "realemail_812@outlook.edu.au",
+        "password" : "Password1",
+    }
+    response_login = requests.post(
+        f"{config.url}auth/login/v2",
+        json=data_login
+    )
+    assert response_login.json() == response_register.json()
 
 # Testing handle_str
 
-def test_handle_str(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    user_id = auth_register_v1(email, password, name_first, name_last)['auth_user_id']
-    channel_id = channels_create_v1(user_id, "channel_name", is_public=True)['channel_id']
-    expected = {
-        'name' : "channel_name",
-        'is_public' : True,
-        'owner_members' : [
-            {
-                'u_id' : user_id,
-                'email' : "realemail_812@outlook.edu.au",
-                'name_first' : "John",
-                'name_last' : "Smith",
-                'handle_str' : "johnsmith"
-            },
-        ],
-        'all_members' : [
-            {
-                'u_id' : user_id,
-                'email' : "realemail_812@outlook.edu.au",
-                'name_first' : "John",
-                'name_last' : "Smith",
-                'handle_str' : "johnsmith"
-            },
-        ],
-    }
-    assert channel_details_v1(user_id, channel_id) == expected
+## MUST IMPLEMENT DETAILS ROUTE FIRST
+
+# def test_handle_str(reset_data):
+#     data_register = {
+#         "email": "realemail_812@outlook.edu.au",
+#         "password": "Password1",
+#         "name_first": "John",
+#         "name_last": "Smith",
+#     }
+#     response_register = requests.post(
+#         f"{config.url}auth/register/v2",
+#         json=data_register
+#     )
+#     user_id = response_register.json()['auth_user_id']
+#     data_create = {
+#         'auth_user_id': user_id,
+#         'name': "channel_name",
+#         'is_public': True,
+#     }
+#     response_create = requests.post(
+#         f"{config.url}channels/create/v2",
+#         json=data_create
+#     )
+#     channel_id = response_create.json()['channel_id']
+#     data_details = {
+#         'auth_user_id': user_id,
+#         'channel_id': channel_id,
+#     }
+#     response_details = requests.post(
+#         f"{config.url}channel/details/v2",
+#         json=data_details
+#     )
+#     expected = {
+#         'name' : "channel_name",
+#         'is_public' : True,
+#         'owner_members' : [
+#             {
+#                 'email' : "realemail_812@outlook.edu.au",
+#                 'name_first' : "John",
+#                 'name_last' : "Smith",
+#                 'handle_str' : "johnsmith",
+#             },
+#         ],
+#         'all_members' : [
+#             {
+#                 'email' : "realemail_812@outlook.edu.au",
+#                 'name_first' : "John",
+#                 'name_last' : "Smith",
+#                 'handle_str' : "johnsmith",
+#             },
+#         ],
+#     }
+#     assert response_details.json() == expected
+
+# def test_handle_str_repeated(reset_data):
+#     data_register = {
+#         "email": "realemail_812@outlook.edu.au",
+#         "password": "Password1",
+#         "name_first": "John",
+#         "name_last": "Smith",
+#     }
+#     requests.post(f"{config.url}auth/register/v2", json=data_register)
+#     data_register2 = {
+#         "email": "realemail_289@outlook.edu.au",
+#         "password": "Password1",
+#         "name_first": "John",
+#         "name_last": "Smith",
+#     }
+#     response_register2 = requests.post(
+#         f"{config.url}auth/register/v2",
+#         json=data_register2
+#     )
+#     user_id2 = response_register2.json()['auth_user_id']
+#     data_create = {
+#         'auth_user_id': user_id2,
+#         'name': "channel_name",
+#         'is_public': True,
+#     }
+#     response_create = requests.post(
+#         f"{config.url}channels/create/v2",
+#         json=data_create
+#     )
+#     channel_id = response_create.json()['channel_id']
+#     data_details = {
+#         'auth_user_id': user_id2,
+#         'channel_id': channel_id,
+#     }
+#     response_details = requests.post(
+#         f"{config.url}channel/details/v2",
+#         json=data_details
+#     )
+#     expected = {
+#         'name' : "channel_name",
+#         'is_public' : True,
+#         'owner_members' : [
+#             {
+#                 'email' : "realemail_289@outlook.edu.au",
+#                 'name_first' : "John",
+#                 'name_last' : "Smith",
+#                 'handle_str' : "johnsmith0",
+#             },
+#         ],
+#         'all_members' : [
+#             {
+#                 'email' : "realemail_289@outlook.edu.au",
+#                 'name_first' : "John",
+#                 'name_last' : "Smith",
+#                 'handle_str' : "johnsmith0",
+#             },
+#         ],
+#     }
+#     assert response_details.json() == expected
 
 # Tests for error checking auth_register
 
 def test_invalid_email(reset_data):
-    email = "uhh, im also a real email?"
-    password = "asdfghjkl"
-    name_first = "Bill"
-    name_last = "Thompson"
-    with pytest.raises(InputError):
-        auth_register_v1(email, password, name_first, name_last)
+    data_register = {
+        "email": "uhh, im also a real email?",
+        "password": "asdfghjkl",
+        "name_first": "Bill",
+        "name_last": "Thompson",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    assert response_register.status_code == 400
 
 def test_email_repeat(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    auth_register_v1(email, password, name_first, name_last)
-    with pytest.raises(InputError):
-        auth_register_v1(email, password, name_first, name_last)
+    data_register = {
+        "email": "realemail_812@outlook.edu.au",
+        "password": "Password1",
+        "name_first": "John",
+        "name_last": "Smith",
+    }
+    data_login = {
+        "email": "realemail_812@outlook.edu.au",
+        "password": "Password1",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    response_login = requests.post(
+        f"{config.url}auth/login/v2",
+        json=data_login
+    )
+    assert response_login.json() == response_register.json()
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    assert response_register.status_code == 400
 
 def test_short_password(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "short"
-    name_first = "John"
-    name_last = "Smith"
-    with pytest.raises(InputError):
-        auth_register_v1(email, password, name_first, name_last)
+    data_register = {
+        "email": "realemail_812@outlook.edu.au",
+        "password": "short",
+        "name_first": "John",
+        "name_last": "Smith",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    assert response_register.status_code == 400
 
 def test_long_firstname(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John" * 20
-    name_last = "Smith"
-    with pytest.raises(InputError):
-        auth_register_v1(email, password, name_first, name_last)
+    data_register = {
+        "email": "realemail_812@outlook.edu.au",
+        "password": "Password1",
+        "name_first": "John" * 20,
+        "name_last": "Smith",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    assert response_register.status_code == 400
 
 def test_long_lastname(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith" * 20
-    with pytest.raises(InputError):
-        auth_register_v1(email, password, name_first, name_last)
+    data_register = {
+        "email": "realemail_812@outlook.edu.au",
+        "password": "Password1",
+        "name_first": "John",
+        "name_last": "Smith" * 20,
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    assert response_register.status_code == 400
 
 # Test for error checking auth_login
 
 def test_wrong_password(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    auth_register_v1(email, password, name_first, name_last)
+    data_register = {
+        "email" : "realemail_812@outlook.edu.au",
+        "password" : "Password1",
+        "name_first" : "John",
+        "name_last" : "Smith",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
     
-    password = "wrong_password"
-    with pytest.raises(InputError):
-        auth_login_v1(email, password)
+    data_login = {
+        "email" : "realemail_812@outlook.edu.au",
+        "password": "wrong_password",
+    }
+    response_login = requests.post(
+        f"{config.url}auth/login/v2",
+        json=data_login
+    )
+    assert response_login.status_code == 400
 
 def test_wrong_email(reset_data):
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    auth_register_v1(email, password, name_first, name_last)
-    
-    email = "wrong_email@outlook.com"
-    with pytest.raises(InputError):
-        auth_login_v1(email, password)
+    data_register = {
+        "email": "realemail_812@outlook.edu.au",
+        "password": "Password1",
+        "name_first": "John",
+        "name_last": "Smith",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    data_login = {
+        "email": "wrong_email@outlook.com",
+        "password": "Password1",
+    }
+    response_login = requests.post(
+        f"{config.url}auth/login/v2",
+        json=data_login
+    )
+    assert response_login.status_code == 400
