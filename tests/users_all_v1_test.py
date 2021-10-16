16f99ec5c57c69f838c314b6fa31267447914861
z5360742 <z5360742@cse.unsw.edu.au>
2021-10-16 22:27:02 +1100

    Wrote some tests for users/all/v1

---
+++ tests/users_all_v1_test.py
@@ -0,0 +1,101 @@
+import pytest
+import requests
+from src import config 
+
+@pytest.fixture 
+def reset(): 
+    requests.delete(f"{config.url}clear/v1")
+
+    data_register = {
+        "email" : "realemail_812@outlook.edu.au",
+        "password" : "Password1",
+        "name_first" : "John",
+        "name_last" : "Smith",
+    }
+
+    requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+
+    data_login = { 
+        'email': 'realemail_812@outlook.edu.au', 
+        'password': 'Password1', 
+    }
+    
+    response = requests.post(f'{config.url}auth/login/v2', json=data_login)
+
+    return response.json()
+
+def test_one_valid(reset): 
+    data_user = {
+        'token': reset['token'], 
+    } 
+    response = requests.get(f'{config.url}users/all/v1', json=data_user)
+
+    assert response.json() == {'users': [{
+        'u_id': reset['auth_user_id'], 
+        'email': 'realemail_812@outlook.edu.au', 
+        'name_first': 'John', 
+        'name_last': 'Smith', 
+        'handle_str': 'johnsmith', 
+    }]}
+
+def test_two_valid(reset): 
+    u_id_1 = reset['auth_user_id']
+
+    data_register = {
+        "email" : "realemail_813@outlook.edu.au",
+        "password" : "Password2",
+        "name_first" : "Tim",
+        "name_last" : "Lee",
+    }
+
+    requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+
+    data_login = { 
+        'email': 'realemail_813@outlook.edu.au', 
+        'password': 'Password2', 
+    }
+    
+    response = requests.post(f'{config.url}auth/login/v2', json=data_login)
+    response_data = response.json()
+    u_id_2 = response_data['auth_user_id']
+
+    data_user = { 
+        'token': response_data['token'], 
+    }
+    response = requests.get(f'{config.url}users/all/v1', json=data_user)
+
+    users_list = [{
+        'u_id': u_id_1, 
+        'email': 'realemail_812@outlook.edu.au', 
+        'name_first': 'John', 
+        'name_last': 'Smith', 
+        'handle_str': 'johnsmith'
+    }, 
+    {
+        'u_id': u_id_2, 
+        'email': 'realemail_813@outlook.edu.au', 
+        'name_first': 'Tim', 
+        'name_last': 'Lee', 
+        'handle_str': 'timlee'
+    }]
+
+    # sort both lists by u_id to allow direct comparison 
+    sorted_users_list = sorted(users_list, key = lambda k: k['u_id']) 
+    sorted_response_list = sorted(response.json()['users'], key = lambda k: k['u_id']) 
+
+    assert sorted_users_list == sorted_response_list 
+
+def test_invalid_user(reset): 
+    data_user = {
+        'token': '',
+    }
+    response = requests.get(f'{config.url}users/all/v1', json=data_user)
+
+    assert response.status_code == 403 
+
