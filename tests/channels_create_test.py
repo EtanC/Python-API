dd15bb2645364f3cc730bd1d723bd1927bd4d7a5
z5360742 <z5360742@cse.unsw.edu.au>
2021-10-17 21:02:34 +1100

    Implemented tests for channels_create_v2, require more tests using channels join and channel details later

--- tests/channels_create_test.py
+++ tests/channels_create_test.py
@@ -1,150 +1,82 @@
-import pytest
+import pytest 
+import requests
+from src import config 
 
-from src.channels import channels_create_v1 
-from src.channel import channel_details_v1
-from src.auth import auth_register_v1, auth_login_v1 
-from src.other import clear_v1
-from src.error import InputError 
-from src.error import AccessError 
 
-# Clear all data before testing 
-# register and log in before testing 
-@pytest.fixture 
+@pytest.fixture
 def reset(): 
-    clear_v1() 
- 
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    auth_register_v1(email, password, name_first, name_last)
-    result = auth_login_v1(email, password) 
-    # take auth user id from returned dictionary 
-    auth_user_id = result['auth_user_id']
-    return auth_user_id 
+    requests.delete(f"{config.url}clear/v1")
+    
+    data_register = {
+        "email" : "realemail_812@outlook.edu.au",
+        "password" : "Password1",
+        "name_first" : "John",
+        "name_last" : "Smith",
+    }
+
+    response = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    '''
+    data_login = { 
+        "email": "realemail_812@outlook.edu.au", 
+        "password": "Password1", 
+    }
+    
+    response = requests.post(f'{config.url}auth/login/v2', json=data_login)
+    ''' 
+    response_data = response.json()
+    return response_data['token']
+
 
 def test_valid(reset): 
-    # auth_user_id = return value from reset 
-    auth_user_id = reset 
+    data_create = {
+        "token": reset, 
+        "name": "channel1", 
+        "is_public": True,
+    }
 
-    channel_name = "channel1_"
-    is_public = True 
-    result = channels_create_v1(auth_user_id, channel_name, is_public)
-    # take channel id from returned dictionary and check if its an int 
-    channel_id = result['channel_id']
+    response_create = requests.post(f"{config.url}channels/create/v2",\
+        json=data_create) 
+    response_data = response_create.json()
+
+    channel_id = response_data['channel_id'] 
     assert type(channel_id) is int 
 
-def test_valid_store(reset): 
-    auth_user_id = reset 
-    channel_name = "channel1_"
-    is_public = True 
-    result = channels_create_v1(auth_user_id, channel_name, is_public)
-    channel_id = result['channel_id']
-
-    assert channel_details_v1(auth_user_id, channel_id) == \
-    { 
-        'name': channel_name, 
-        'is_public': is_public, 
-        'owner_members': [
-            { 
-                'u_id': auth_user_id, 
-                'email': "realemail_812@outlook.edu.au", 
-                'name_first': "John", 
-                'name_last': "Smith", 
-                'handle_str': "johnsmith"
-            }
-        ], 
-        'all_members': [
-            { 
-                'u_id': auth_user_id, 
-                'email': "realemail_812@outlook.edu.au", 
-                'name_first': "John", 
-                'name_last': "Smith", 
-                'handle_str': "johnsmith"
-            }
-        ],
-    }
-
-def test_multiple_create(reset): 
-    auth_user_id = reset 
-    channel_name = "channel1_"
-    is_public = True 
-    result = channels_create_v1(auth_user_id, channel_name, is_public)
-    channel_id = result['channel_id']
-
-    channel_name2 = "channel2" 
-    result = channels_create_v1(auth_user_id, channel_name2, is_public) 
-    channel_id2 = result['channel_id']
-
-    assert channel_details_v1(auth_user_id, channel_id) == \
-    { 
-        'name': channel_name, 
-        'is_public': is_public, 
-        'owner_members': [
-            { 
-                'u_id': auth_user_id, 
-                'email': "realemail_812@outlook.edu.au", 
-                'name_first': "John", 
-                'name_last': "Smith", 
-                'handle_str': "johnsmith"
-            }
-        ], 
-        'all_members': [
-            { 
-                'u_id': auth_user_id, 
-                'email': "realemail_812@outlook.edu.au", 
-                'name_first': "John", 
-                'name_last': "Smith", 
-                'handle_str': "johnsmith"
-            }
-        ],
-    }
-
-    assert channel_details_v1(auth_user_id, channel_id2) == \
-        { 
-        'name': channel_name2, 
-        'is_public': is_public, 
-        'owner_members': [
-            { 
-                'u_id': auth_user_id, 
-                'email': "realemail_812@outlook.edu.au", 
-                'name_first': "John", 
-                'name_last': "Smith", 
-                'handle_str': "johnsmith"
-            }
-        ], 
-        'all_members': [
-            { 
-                'u_id': auth_user_id, 
-                'email': "realemail_812@outlook.edu.au", 
-                'name_first': "John", 
-                'name_last': "Smith", 
-                'handle_str': "johnsmith"
-            }
-        ],
-    }
-
 def test_short_name(reset): 
+    data_create = { 
+        'token': reset,
+        'name': '', 
+        'is_public': True, 
+    }
 
-    auth_user_id = reset 
-
-    channel_name = "" 
-    is_public = True
-    with pytest.raises(InputError):
-        channels_create_v1(auth_user_id, channel_name, is_public)
+    response_create = requests.post(f"{config.url}channels/create/v2",\
+        json=data_create) 
+    
+    assert response_create.status_code == 400
 
 def test_long_name(reset): 
-
-    auth_user_id = reset 
-
-    channel_name = "hello" * 20 
-    is_public = True 
-    with pytest.raises(InputError): 
-        channels_create_v1(auth_user_id, channel_name, is_public)
+    data_create = { 
+        'token': reset, 
+        'name': 'hello'*20, 
+        'is_public': True, 
+    }
+    
+    response_create = requests.post(f"{config.url}channels/create/v2",\
+        json=data_create) 
+    
+    assert response_create.status_code == 400 
 
 def test_invalid_user(reset): 
-    auth_user_id = reset + 1
-    channel_name = "channel1_"
-    is_public = True
-    with pytest.raises(AccessError): 
-        channels_create_v1(auth_user_id, channel_name, is_public)
+    data_create = { 
+        'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiS2V2aW4ifQ.kEg0Lcmdnk9a5WrUhfSi3F7hRsEHk5-7u7bZ9s49paA', 
+        'name': 'channel1', 
+        'is_public': True, 
+    }
+
+    response_create = requests.post(f"{config.url}channels/create/v2",\
+        json=data_create) 
+    
+    assert response_create.status_code == 403 
+
