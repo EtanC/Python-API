b34ba8e52a601e01cdb4dd0c3c230d066f0b38b8
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-17 23:29:41 +1100

    added tests for message/send/v1

--- tests/message_test.py
+++ tests/message_test.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 import json
 from src.channels import channels_create_v1, channels_create_v2
+from datetime import timezone, datetime
 
 from src import config
 '''
@@ -13,8 +14,51 @@ url = f"http://localhost:{port}/"
 def reset_data():
     requests.delete(f"{config.url}clearmessage/v1")
 
-def test_():
-    pass
+@pytest.fixture
+def user1():
+    data_register = {
+        'email': "harry.williams@gmail.com",
+        'password': "password_harry",
+        'name_first': "Harry",
+        'name_last': "Williams",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    auth_user_id = response_register.json()['auth_user_id']
+    token = response_register.json()['token']
+    return {'token' : token, 'auth_user_id' : auth_user_id}
+
+@pytest.fixture
+def user2():
+    data_register = {
+        'email': "michael.dawson1@gmail.com",
+        'password': "michaeldawson",
+        'name_first': "Michael",
+        'name_last': "Dawson",
+    }
+    response_register = requests.post(
+        f"{config.url}auth/register/v2",
+        json=data_register
+    )
+    auth_user_id = response_register.json()['auth_user_id']
+    token = response_register.json()['token']
+    return {'token' : token, 'auth_user_id' : auth_user_id}
+
+@pytest.fixture
+def channel1(user1):
+    data_create = {
+        'auth_user_id': user1,
+        'name': "Channel1",
+        'is_public': True,
+    }
+    response_create = requests.post(
+        f"{config.url}channels/create/v2",
+        json=data_create
+    )
+    channel_id = response_create.json()['channel_id']
+    return {'user_id' : user1['auth_user_id'], 'channel_id' : channel_id}
 
 #EXAMPLE:
 '''
@@ -41,7 +85,7 @@ def test_invalid_length_message(reset_data): #POST
     json=data_register)
     assert response_register.status_code == 400
 
-    # >1000 length message
+    # >1000 length message (InputError)
     data_register['message'] = 'x' * 1001
     response_register = requests.post(f"{config.url}message/send/v1",\
     json=data_register)
@@ -49,24 +93,80 @@ def test_invalid_length_message(reset_data): #POST
 
 def test_invalid_channelID_message(reset_data): #POST
 
-    # get channel_id from channels/create/v2 (returns that)
-    # channel_id = channels_details_v2(token, name, is_public)
-    # data_register = {
-    #    "token": "TOKEN",
-    #    "message": "valid_message",
-    # }
-    # data_register.update(channel_id) 
-    # invalid channel ID
-    # response_register = requests.post(f"{config.url}message/send/v1",\
-    # json=data_register)
-    # assert response_register.status_code == 400
+   #  get channel_id from channels/create/v2 (returns that)
 
+    channel_id = channels_create_v2(token, name, is_public)
+
+    data_register = {
+      "token": "TOKEN",
+       "message": "valid_message",
+    }
+    data_register.update(channel_id) 
+    data_register['channel_id'] += 1
+
+    # invalid channel ID test (InputError)
+    response_register = requests.post(f"{config.url}message/send/v1",\
+    json=data_register)
+    assert response_register.status_code == 400
+
+def test_invalid_token_message(reset_data): #POST
     pass
 
-def test_nonmember_channel_message(reset_data):
-    pass
+def test_nonmember_channel_message(reset_data, channel1, user2): # POST
 
+    data_register = {
+       "token": user2['token'],
+       "message": "valid_message",
+       "channel_id": channel1['channel_id']
+    }
 
+    response_register = requests.post(f"{config.url}message/send/v1",\
+    json=data_register)
+    assert response_register.status_code == 403
+
+def test_valid_send_message(reset_data, channel1): #POST
+
+    data_send_message = {
+       #"token": user['token'], PLACEHOLDER - token not done yet
+       "message": "valid_message",
+       "channel_id": channel1['channel_id']
+    }
+
+    response_send_message = requests.post(
+        f"{config.url}message/send/v1",
+        json=data_send_message
+    )
+    response_send_message_data = response_send_message.json()
+
+    dt = datetime.now()
+    time_created = dt.replace(tzinfo=timezone.utc).timestamp()
+
+    expected = {
+        'messages': [
+        {
+            'message_id': response_send_message_data['message_id'],
+            'u_id': channel1['user_id'],
+            'message': "valid_message",
+            'time_created': time_created,
+        }
+    ], 
+        'start': 0,
+        'end': -1 # -1 : no more messages to load
+
+    }
+
+    data_details = {
+        #"token": user2['token'], PLACEHOLDER - token not done yet
+        'channel_id': channel1['channel_id'],
+        'start': 0  # 0 = first message sent
+    } 
+
+    response_channel_messages_details = requests.get(
+        f"{config.url}channel/messages/v2",
+        json=data_details
+    )
+
+    assert response_channel_messages_details.json() == expected
 
 
 
