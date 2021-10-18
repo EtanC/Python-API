f90b8a861656a3e2882f45a76dc20d7d5407765e
Braian <z5358768@ad.unsw.edu.au>
2021-10-18 11:16:08 +1100

    Modified channel_messages_v1 tests into http tests, need channels_create to pass pytest

--- tests/channels_messages_test.py
+++ tests/channels_messages_test.py
@@ -1,41 +1,67 @@
-from src.channel import channel_messages_v1
-from src.channels import channels_create_v1
-from src.auth import auth_register_v1
-from src.error import InputError, AccessError
-from src.other import clear_v1
 import pytest
+import requests
+from src import config
 
 @pytest.fixture
 def reset_data():
-    clear_v1()
+    requests.delete(f'{config.url}clear/v1')
 
 @pytest.fixture
-def messages_data():
-    email = "realemail_812@outlook.edu.au"
-    password = "Password1"
-    name_first = "John"
-    name_last = "Smith"
-    user_id = auth_register_v1(email, password, name_first, name_last)['auth_user_id']
-    channel_name = "Channel1"
-    channel_id = channels_create_v1(user_id, channel_name, False)['channel_id']
-    return (user_id, channel_id)
+def user1():
+    data_register = {
+        'email': 'realemail_812@outlook.edu.au',
+        'password': 'Password1',
+        'name_first': 'John',
+        'name_last': 'Smith',
+    }
+    response_register = requests.post(
+        f'{config.url}auth/register/v2',
+        json=data_register
+    )
+    return response_register.json()['auth_user_id']
 
 @pytest.fixture
-def extra_user():
-    email = "realemail_127@outlook.edu.au"
-    password = "Password1"
-    name_first = "Smith"
-    name_last = "John"
-    another_user_id = auth_register_v1(email, password, name_first, name_last)['auth_user_id']
-    return another_user_id
+def user2():
+    data_register2 = {
+        'email': 'realemail_127@outlook.edu.au',
+        'password': 'Password1',
+        'name_first': 'Smith',
+        'name_last': 'John',
+    }
+    response_register2 = requests.post(
+        f'{config.url}auth/register/v2',
+        json=data_register2
+    )
+    return response_register2.json()['auth_user_id']
+
+@pytest.fixture
+def channel1(user1):
+    data_create = {
+        'auth_user_id': user1,
+        'name': 'Channel1',
+        'is_public': True,
+    }
+    response_create = requests.post(
+        f'{config.url}channels/create/v2',
+        json=data_create
+    )
+    channel_id = response_create.json()['channel_id']
+    return {'user_id' : user1, 'channel_id' : channel_id}
 
 
 # Testing valid 
 
-def test_valid(reset_data, messages_data):
-    user_id = messages_data[0]
-    channel_id = messages_data[1]
-    assert channel_messages_v1(user_id, channel_id, 0) == {'messages' : [], 'start': 0, 'end': -1}
+def test_valid(reset_data, channel1):
+    data_messages = {
+        'auth_user_id' : channel1['user_id'],
+        'channel_id' : channel1['channel_id'],
+        'start' : 0,
+    }
+    response_messages = requests.get(
+        f'{config.url}channel/messages/v1',
+        json=data_messages
+    )
+    assert response_messages.json() == {'messages' : [], 'start': 0, 'end': -1}
 
 # def test_valid_nonowner(reset_data, messages_data, extra_user):
 #     user_id = messages_data[0]
@@ -46,29 +72,53 @@ def test_valid(reset_data, messages_data):
 
 # Testing errors
 
-def test_invalid_channel_id(reset_data, messages_data):
-    user_id = messages_data[0]
-    channel_id = messages_data[1] + 1
-    with pytest.raises(InputError):
-        channel_messages_v1(user_id, channel_id, 0)
+def test_invalid_channel_id_messages(reset_data, channel1):
+    data_messages = {
+        'auth_user_id' : channel1['user_id'],
+        'channel_id' : channel1['channel_id'] + 1,
+        'start' : 0,
+    }
+    response_messages = requests.get(
+        f'{config.url}channel/messages/v1',
+        json=data_messages
+    )
+    assert response_messages.status_code == 400
 
-def test_invalid_start(reset_data, messages_data):
-    user_id = messages_data[0]
-    channel_id = messages_data[1]
-    with pytest.raises(InputError):
-        channel_messages_v1(user_id, channel_id, 1000)
+def test_invalid_start_messages(reset_data, channel1):
+    data_messages = {
+        'auth_user_id' : channel1['user_id'],
+        'channel_id' : channel1['channel_id'],
+        'start' : 1000,
+    }
+    response_messages = requests.get(
+        f'{config.url}channel/messages/v1',
+        json=data_messages
+    )
+    assert response_messages.status_code == 400
 
-def test_invalid_user(reset_data, messages_data, extra_user):
-    channel_id = messages_data[1]
-    another_user_id = extra_user
-    with pytest.raises(AccessError):
-        channel_messages_v1(another_user_id, channel_id, 0)
+def test_invalid_user(reset_data, channel1, user2):
+    data_messages = {
+        'auth_user_id' : channel1['user_id'],
+        'channel_id' : user2,
+        'start' : 0,
+    }
+    response_messages = requests.get(
+        f'{config.url}channel/messages/v1',
+        json=data_messages
+    )
+    assert response_messages.status_code == 400
 
-def test_invalid_user_id(reset_data, messages_data):
-    user_id = messages_data[0] + 1
-    channel_id = messages_data[1]
-    with pytest.raises(AccessError):
-        channel_messages_v1(user_id, channel_id, 0)
+def test_invalid_user_id(reset_data, channel1):
+    data_messages = {
+        'auth_user_id' : channel1['user_id'] + 1,
+        'channel_id' : channel1['channel_id'],
+        'start' : 0,
+    }
+    response_messages = requests.get(
+        f'{config.url}channel/messages/v1',
+        json=data_messages
+    )
+    assert response_messages.status_code == 400
 
 
 ## TODO: test edge cases for the number of messages that are returned
