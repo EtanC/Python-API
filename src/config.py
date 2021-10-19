63ba3be2461d07dd67fe600cef51d4eaf1a21e09
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-19 23:09:56 +1100

    fixed conflicts

--- src/config.py
+++ src/config.py
@@ -1,11 +1,4 @@
 port = 8080
-<<<<<<< HEAD
 EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 SECRET = "L-L>V\\y3f4]fEH\\;haf/"
-=======
-
-EMAIL_REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-SECRET = "L-L>V\\y3f4]fEH\\;haf/"
-
->>>>>>> implented and tested message/send/v1
 url = f"http://localhost:{port}/"
