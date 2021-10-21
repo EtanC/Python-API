a28cf6d94ebd83e7d5058f394489be1e629afe72
z5364292 <z5364292@cse.unsw.edu.au>
2021-10-21 18:31:48 +1100

    added implementations

--- src/server.py
+++ src/server.py
@@ -14,7 +14,8 @@ from src.user import users_all_v1, user_profile_v1, user_profile_setemail_v1, \
 from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
 from src.channel import channel_details_v1, channel_messages_v1, channel_join_v1
 from src.message import message_edit_v1
-
+from src.helper import decode_token 
+from src.dm import dm_create_v2
 def quit_gracefully(*args):
     '''For coverage'''
     exit(0)
@@ -429,6 +430,38 @@ def clear():
     '''
     clear_v1()
     return dumps({})
+
+
+'''
+
+dms.py section 
+
+'''
+
+
+@APP.route("/dms/create/v2", methods=['POST'])
+def dm_create_v2(): 
+    '''
+    Given a channel with ID channel_id that the authorised user is a member of, 
+    provide basic details about the channel 
+
+    Arguments:
+        token (str): token identifying user
+        u_ids (list): list of u_id 
+        
+    Exceptions: 
+        InputError  - Invalid u_id in the list of u_ids
+
+    Returns: 
+        Returns {dms} on successful creation 
+    '''
+
+    data = request.get_json() 
+
+    return_dict = dm_create_v2(data['token'], data['u_ids'])
+    
+    return dumps(return_dict) 
+
 
 
 #### NO NEED TO MODIFY BELOW THIS POINT
