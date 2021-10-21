e7f7c550be7a3b756ac2147983b10c2c541c83ff
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-21 20:15:22 +1100

    fixed helper

--- src/server.py
+++ src/server.py
@@ -13,7 +13,7 @@ from src.user import users_all_v1, user_profile_v1, user_profile_setemail_v1, \
 
 from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
 from src.channel import channel_details_v1, channel_messages_v1, channel_join_v1
-from src.message import message_edit_v1
+from src.message import message_edit_v1, message_send_v1
 from src.helper import decode_token 
 from src.dm import dm_create_v2
 
