b551e8ac85113bf97dfe468d26ab5b3d01394146
Kevin Chen <kevin.chen10@student.unsw.edu.au>
2021-10-22 22:41:30 +1100

    Revert "Merge branch 'persistence_implementation' into 'master'"
    This reverts merge request !60

--- src/other.py
+++ src/other.py
@@ -4,5 +4,7 @@ def clear_v1():
     store = data_store.get()
     store['users'] = []
     store['channels'] = []
+    store['handle_append_no'] = {}
+    store['user_id_number'] = 1
     store['dms'] = []
     data_store.set(store)
