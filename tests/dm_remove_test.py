2a9ff62336333f58be076c3a2aebe3aa12ce3ba8
z5364292 <z5364292@cse.unsw.edu.au>
2021-10-22 23:10:06 +1100

    added implmenetations but need to be fixed

--- tests/dm_remove_test.py
+++ tests/dm_remove_test.py
@@ -27,10 +27,15 @@ def reset():
         f"{config.url}auth/register/v2",
         json=register_receiver
     )
+    u_id = response_receiver.json()['auth_user_id']
 
+    data = {
+        'token' : response_owner.json()['token'], 
+        'u_ids' : [u_id]
+    }
     response_create= requests.post(
         f"{config.url}dm/create/v1",
-        json=register_receiver
+        json=data
     )
     return (response_owner.json(), response_receiver.json(), response_create.json())
 
@@ -44,7 +49,6 @@ def test_return_type(reset):
         json=data
     )
     assert type(response.json()) is dict
-    assert response.json() == {}
 
 def test_invalid_id(reset): 
     data = {
@@ -55,30 +59,19 @@ def test_invalid_id(reset):
         f"{config.url}dm/remove/v1",
         json=data
     )
-    assert response.json() == 400
+    assert response.status_code == 400
 
 def test_Access_error(reset): 
     data = {
-        'token' : reset[1]['token'], 
-        'dm_id' : reset[2]['dm_id'] + 1
-    }
-    response = requests.delete(
-        f"{config.url}dm/remove/v1",
-        json=data
-    )
-    assert response.json() == 403
-
-def test_empty(reset):
-    # also access error since the dm does not exist the owner does not exist
-    data = {
-        'token' : reset[0]['token'], 
+        'token' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiS2V2aW4ifQ.kEg0Lcmdnk9a5WrUhfSi3F7hRsEHk5-7u7bZ9s49paA',
         'dm_id' : reset[2]['dm_id']
     }
     response = requests.delete(
         f"{config.url}dm/remove/v1",
         json=data
     )
-    assert response.json() == 403
+    assert response.status_code == 403
+
 
 def test_short(reset): 
     data = {
@@ -91,16 +84,18 @@ def test_short(reset):
     )
 
     data = {
-        'token': reset[1]['token']
+        'token': reset[1]['token'],
+        'dm_id' : reset[2]['dm_id']
     }
     response = requests.get(
         f"{config.url}dm/details/v1",
         params=data
     )
-    assert response == \
+    assert response.json() == \
         {
+            'members': [],  
             'name': 'abbysmith', 
-            'members': [{}]
+            
         }
 
     
