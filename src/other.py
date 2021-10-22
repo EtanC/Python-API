9f61bbca2f4605645000b6f9978f1ff6089631ef
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-23 01:43:16 +1100

    fixed clear_v1

--- src/other.py
+++ src/other.py
@@ -4,7 +4,7 @@ def clear_v1():
     store = data_store.get()
     store['users'] = []
     store['channels'] = []
-    store['handle_append_no'] = {}
+    #store['handle_append_no'] = {}
     #store['user_id_number'] = 1
     #store['message_id'] = 1
     store['dms'] = []
