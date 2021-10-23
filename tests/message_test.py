8f658c854be7ca7565c3ce51443e740d50110986
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-23 17:19:10 +1100

    fixed params

--- tests/message_test.py
+++ tests/message_test.py
@@ -235,7 +235,7 @@ def test_valid_message_edit(reset_data, user1, channel1): #PUT
     }
 
     response_channel_messages_data =requests.get(f"{config.url}channel/messages/v2", \
-        json=channel_messages)
+        params=channel_messages)
 
     response_data = response_channel_messages_data.json()
     
