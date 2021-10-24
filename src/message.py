32771e7612e669df0857d7ca0907b25df91bf5d3
Benjamin Hung <z5311811@ad.unsw.edu.au>
2021-10-25 01:14:56 +1100

    fixed pylint

--- src/message.py
+++ src/message.py
@@ -66,6 +66,42 @@ def message_senddm_v1(token, dm_id, message):
         'message_id': message_id
     }
 
+def message_remove_v1(token, message_id):
+       
+    # if token is invalid or doesn't have an 'auth_user_id' which it should 
+    token_data = decode_token(token)
+    if (token_data is None) or ('auth_user_id' not in token_data): 
+        raise AccessError(description='Invalid token')
+
+    auth_user_id = token_data['auth_user_id']
+    store = data_store.get()
+    user = token_to_user(token, store)
+    message_to_remove = get_message(message_id, store)
+
+    # Checking if auth_user_id is valid
+    if get_user(auth_user_id, store) == None:
+        raise AccessError(description="auth_user_id is not valid")
+
+    # check the token's validity:
+    if user == None:
+        raise AccessError(description="INVALID token passed in")
+
+    # check message ID validity:
+    if get_message(message_id, store) == None:
+        raise InputError(description="message ID is INVALID")
+
+    # check if user is allowed to edit the right message (might need to change for the owener permissions)
+    if auth_user_id != message_to_remove['u_id']:
+        raise AccessError(description="User is NOT AUTHORISED to edit message")
+
+    # remove the message
+    for i,channel in enumerate(store['channels']):
+        for j,message in enumerate(channel['messages']):
+            if message['message_id'] == message_id:
+                del store['channels'][i]['messages'][j]
+
+    return {}
+
 def message_edit_v1(token, message_id, message):
        
     # if token is invalid or doesn't have an 'auth_user_id' which it should 
