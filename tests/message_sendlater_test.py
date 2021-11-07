80b1002f62a4cf2acd7e1b284816cbf5ab20f279
z5360742 <z5360742@cse.unsw.edu.au>
2021-11-07 21:30:42 +1100

    Added a test checking interaction if a message is sent quickly before a previous message in sendlater arrives

--- tests/message_sendlater_test.py
+++ tests/message_sendlater_test.py
@@ -31,6 +31,7 @@ def reset():
 
 def test_valid(reset): 
     # time_sent = current time + 3 seconds
+    # send message after 3 secs
     time_sent = datetime.now().replace(tzinfo=timezone.utc).timestamp() + 3
     data_sendlater = {
         'token': reset['user']['token'], 
@@ -87,6 +88,7 @@ def test_valid(reset):
 
 def test_two_messages(reset): 
     # time_sent = current time + 3 seconds
+    # send message 3 secs after
     time_sent = datetime.now().replace(tzinfo=timezone.utc).timestamp() + 3
     data_sendlater = {
         'token': reset['user']['token'], 
@@ -110,7 +112,7 @@ def test_two_messages(reset):
     response_sendlater_2 = requests.post(f"{config.url}message/sendlater/v1", json=data_sendlater)
     assert type(response_sendlater_2.json()['message_id']) is int
 
-    # sleep 2 seconds and make sure first message isn't there yet
+    # sleep 2 seconds and make sure first and second messages aren't there yet
     time.sleep(2)
     data_messages = { 
         'token': reset['user']['token'], 
@@ -130,7 +132,7 @@ def test_two_messages(reset):
     }
 
     response_messages = requests.get(f"{config.url}channel/messages/v2", params=data_messages)
-    
+    # check timing of message 1 
     channel_messages = response_messages.json()
     current_time = datetime.now().replace(tzinfo=timezone.utc).timestamp()
     assert abs(
@@ -158,13 +160,14 @@ def test_two_messages(reset):
 
     response_messages = requests.get(f"{config.url}channel/messages/v2", params=data_messages)
     
+    # check timing of 2nd message 
     channel_messages = response_messages.json()
     current_time = datetime.now().replace(tzinfo=timezone.utc).timestamp()
     assert abs(
         channel_messages['messages'][1]['time_created'] - current_time
     ) < 2
 
-    # Checking the rest of the return
+    # Checking the second message
     expected = {
         'messages' : [
             {
@@ -261,3 +264,90 @@ def test_invalid_token(reset):
     response_sendlater = requests.post(f"{config.url}message/sendlater/v1", json=data_sendlater)
     assert response_sendlater.status_code == 403
 
+
+def test_send_message_before_sendlater(reset): 
+    # CLARIFICATION: this test is meant to test the message_id's and their interaction
+    # when a message is set to sendlater but another message is quickly sent 
+    # before the message in sendlater has been sent 
+
+    # time_sent = current time + 3 seconds
+    # send a message after 3 secs
+    time_sent = datetime.now().replace(tzinfo=timezone.utc).timestamp() + 3
+    data_sendlater = {
+        'token': reset['user']['token'], 
+        'channel_id': reset['channel_id'],
+        'message': 'valid message', 
+        'time_sent': time_sent,
+    }
+
+    response_sendlater = requests.post(f"{config.url}message/sendlater/v1", json=data_sendlater)
+    assert type(response_sendlater.json()['message_id']) is int
+
+    # send a quick message before prev message arrives
+    data_send = {
+        'token': reset['user']['token'], 
+        'channel_id': reset['channel_id'],
+        'message': 'quick message'
+    }
+    response_send = requests.post(f"{config.url}message/send/v1", json=data_send)
+
+    data_messages = { 
+        'token': reset['user']['token'], 
+        'channel_id': reset['channel_id'],
+        'start': 0, 
+    }
+
+    response_messages = requests.get(f"{config.url}channel/messages/v2", params=data_messages)
+    
+    current_time = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+    channel_messages = response_messages.json()
+    
+    # check timing of message 2
+    assert abs(
+        channel_messages['messages'][0]['time_created'] - current_time
+    ) < 2
+
+    # removing time of 2nd message
+    del channel_messages['messages'][0]['time_created']
+
+
+    quick_message = { 
+        'message_id': response_send.json()['message_id'],
+        'u_id': reset['user']['auth_user_id'], 
+        'message': 'quick message'
+    }
+
+    assert channel_messages == {'messages' : [quick_message], 'start': 0, 'end': -1}
+
+    # sleep for 3.5 secs and check if first message is there
+    time.sleep(3.5)
+    data_messages = { 
+        'token': reset['user']['token'], 
+        'channel_id': reset['channel_id'],
+        'start': 0, 
+    }
+
+    response_messages = requests.get(f"{config.url}channel/messages/v2", params=data_messages)
+    
+    channel_messages = response_messages.json()
+    current_time = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+    assert abs(
+        channel_messages['messages'][1]['time_created'] - current_time
+    ) < 2
+
+    # Checking the rest of the return
+    expected = {
+        'messages' : [ quick_message,
+            {
+                'message_id' : response_sendlater.json()['message_id'],
+                'u_id' : reset['user']['auth_user_id'],
+                'message' : 'valid message',
+            }, 
+        ],
+        'start' : 0,
+        'end' : -1,
+    }
+    # Removing time of both 1st and 2nd message
+    del channel_messages['messages'][0]['time_created']
+    del channel_messages['messages'][1]['time_created']
+    assert channel_messages == expected
