534e1a24e2009a2e34eeb78a9c5ffcbb03d3a5f8
Braian <z5358768@ad.unsw.edu.au>
2021-11-08 16:36:26 +1100

    Made sure global owners were only granted owner permissions once they joined the channel, added a test for this and refactored tests

--- tests/channel_removeowner_test.py
+++ tests/channel_removeowner_test.py
@@ -1,86 +1,55 @@
 import pytest
 import requests
 from src import config
+from src.error import InputError, AccessError
+from fake.other import clear
+from fake.auth import auth_register
+from fake.channels import channels_create
+from fake.channel import channel_join, channel_removeowner, channel_addowner, channel_details
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def reset_data():
-    requests.delete(f"{config.url}clear/v1")
+    clear()
 
 @pytest.fixture
 def user1():
-    data_register = {
-        'email': "realemail_812@outlook.edu.au",
-        'password': "Password1",
-        'name_first': "John",
-        'name_last': "Smith",
-    }
-    response_register = requests.post(
-        f"{config.url}auth/register/v2",
-        json=data_register
+    return auth_register(
+        "realemail_812@outlook.edu.au", "Password1", "John", "Smith"
     )
-    return response_register.json()
 
 @pytest.fixture
 def user2():
-    data_register2 = {
-        'email': "realemail_127@outlook.edu.au",
-        'password': "Password1",
-        'name_first': "Smith",
-        'name_last': "John",
-    }
-    response_register2 = requests.post(
-        f"{config.url}auth/register/v2",
-        json=data_register2
+    return auth_register(
+        "realemail_127@outlook.edu.au", "Password1", "Smith", "John"
     )
-    return response_register2.json()
 
 @pytest.fixture
 def user3():
-    data_register2 = {
-        'email': "realemail_372@outlook.edu.au",
-        'password': "Password2",
-        'name_first': "Bob",
-        'name_last': "Bill",
-    }
-    response_register2 = requests.post(
-        f"{config.url}auth/register/v2",
-        json=data_register2
+    return auth_register(
+        "realemail_372@outlook.edu.au", "Password2", "Bob", "Bill"
     )
-    return response_register2.json()
 
 @pytest.fixture
 def channel1(user1):
-    data_create = {
-        'token': user1['token'],
-        'name': "Channel1",
-        'is_public': True,
-    }
-    response_create = requests.post(
-        f"{config.url}channels/create/v2",
-        json=data_create
-    )
-    channel_id = response_create.json()['channel_id']
+    channel_id = channels_create(user1['token'], "Channel1", True)['channel_id']
     return {'user' : user1, 'channel_id' : channel_id}
 
+@pytest.fixture
+def two_member_channel(channel1, user2):
+    channel_join(user2['token'], channel1['channel_id'])
+    return {
+        'owner' : channel1['user'],
+        'member' : user2,
+        'channel_id' : channel1['channel_id']
+    }
 
 @pytest.fixture
 def two_owner_channel(channel1, user2):
-    data_join = {
-        'token': user2['token'],
-        'channel_id': channel1['channel_id'],
-    }
-    requests.post(
-        f"{config.url}channel/join/v2",
-        json=data_join
-    )
-    data_addowner = {
-        'token': channel1['user']['token'],
-        'channel_id': channel1['channel_id'],
-        'u_id': user2['auth_user_id'],
-    }
-    requests.post(
-        f"{config.url}channel/addowner/v1",
-        json=data_addowner
+    channel_join(user2['token'], channel1['channel_id'])
+    channel_addowner(
+        channel1['user']['token'],
+        channel1['channel_id'],
+        user2['auth_user_id']
     )
     return {
         'owner1' : channel1['user'],
@@ -90,23 +59,15 @@ def two_owner_channel(channel1, user2):
 
 # Test valid remove owner
 
-def test_valid_removeowner(reset_data, two_owner_channel):
-    data_removeowner = {
-        'token': two_owner_channel['owner1']['token'],
-        'channel_id': two_owner_channel['channel_id'],
-        'u_id': two_owner_channel['owner2']['auth_user_id'],
-    }
-    requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
+def test_valid_removeowner(two_owner_channel):
+    channel_removeowner(
+        two_owner_channel['owner1']['token'],
+        two_owner_channel['channel_id'],
+        two_owner_channel['owner2']['auth_user_id']
     )
-    data_details = {
-        'token': two_owner_channel['owner1']['token'],
-        'channel_id': two_owner_channel['channel_id'],
-    }
-    response_details = requests.get(
-        f"{config.url}channel/details/v2",
-        params=data_details
+    channel_info = channel_details(
+        two_owner_channel['owner1']['token'],
+        two_owner_channel['channel_id']
     )
     expected = {
         'name': "Channel1",
@@ -137,95 +98,69 @@ def test_valid_removeowner(reset_data, two_owner_channel):
             },
         ],
     }
-    assert response_details.json() == expected
+    assert channel_info == expected
+
+def test_global_owner_permissions_removeowner(user1, user2, user3):
+    channel_id = channels_create(user2['token'], "Channel1", True)['channel_id']
+    # User1 joins channel user2 created, but is not an owner
+    channel_join(user1['token'], channel_id)
+    # User3 and user2 are owners of the channel
+    channel_join(user3['token'], channel_id)
+    channel_addowner(user2['token'], channel_id, user3['auth_user_id'])
+    # Despite not being owner, user1 should be able to remove user3 from owners
+    # Have 2 owners cuz channel must always have 1 owner
+    channel_removeowner(user1['token'], channel_id, user3['auth_user_id'])
+
 
 # Testing invalid remove owner
 
-def test_invalid_token_removeowner(reset_data, two_owner_channel):
-    data_removeowner = {
-        'token': 'invalid_token',
-        'channel_id': two_owner_channel['channel_id'],
-        'u_id': two_owner_channel['owner2'],
-    }
-    response_removeowner = requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
-    )
-    assert response_removeowner.status_code == 403
+def test_invalid_token_removeowner(two_owner_channel):
+    with pytest.raises(AccessError):
+        channel_removeowner(
+            'invalid_token',
+            two_owner_channel['channel_id'],
+            two_owner_channel['owner2']
+        )
 
-def test_invalid_channel_id_removeowner(reset_data, two_owner_channel):
-    data_removeowner = {
-        'token': two_owner_channel['owner1']['token'],
-        'channel_id': two_owner_channel['channel_id'] + 1,
-        'u_id': two_owner_channel['owner2']['auth_user_id'],
-    }
-    response_removeowner = requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
-    )
-    assert response_removeowner.status_code == 400
+def test_invalid_channel_id_removeowner(two_owner_channel):
+    with pytest.raises(InputError):
+        channel_removeowner(
+            two_owner_channel['owner1']['token'],
+            two_owner_channel['channel_id'] + 1,
+            two_owner_channel['owner2']['auth_user_id']
+        )
 
-def test_removing_invalid_user_removeowner(reset_data, two_owner_channel):
-    data_removeowner = {
-        'token': two_owner_channel['owner1']['token'],
-        'channel_id': two_owner_channel['channel_id'],
-        'u_id': two_owner_channel['owner2']['auth_user_id']
-                + two_owner_channel['owner1']['auth_user_id'] + 1,
-    }
-    response_removeowner = requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
-    )
-    assert response_removeowner.status_code == 400
+def test_removing_invalid_user_removeowner(two_owner_channel):
+    with pytest.raises(InputError):
+        channel_removeowner(
+            two_owner_channel['owner1']['token'],
+            two_owner_channel['channel_id'],
+            two_owner_channel['owner2']['auth_user_id'] +
+            two_owner_channel['owner1']['auth_user_id'] + 1,
+        )
 
-def test_removing_non_owner_removeowner(reset_data, channel1, user2):
-    data_join = {
-        'token': user2['token'],
-        'channel_id': channel1['channel_id'],
-    }
-    requests.post(
-        f"{config.url}channel/join/v2",
-        json=data_join
-    )
-    data_removeowner = {
-        'token': channel1['user']['token'],
-        'channel_id': channel1['channel_id'],
-        'u_id': user2['auth_user_id'],
-    }
-    response_removeowner = requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
-    )
-    assert response_removeowner.status_code == 400
+def test_removing_non_owner_removeowner(channel1, user2):
+    channel_join(user2['token'], channel1['channel_id'])
+    with pytest.raises(InputError):
+        channel_removeowner(
+            channel1['user']['token'],
+            channel1['channel_id'],
+            user2['auth_user_id']
+        )
 
-def test_remove_only_owner_removeowner(reset_data, channel1):
-    data_removeowner = {
-        'token': channel1['user']['token'],
-        'channel_id': channel1['channel_id'],
-        'u_id': channel1['user']['auth_user_id'],
-    }
-    response_removeowner = requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
-    )
-    assert response_removeowner.status_code == 400
+def test_remove_only_owner_removeowner(channel1):
+    with pytest.raises(InputError):
+        channel_removeowner(
+            channel1['user']['token'],
+            channel1['channel_id'],
+            channel1['user']['auth_user_id']
+        )
 
-def test_no_owner_permissions_removeowner(reset_data, two_owner_channel, user3):
-    data_join = {
-        'token': user3['token'],
-        'channel_id': two_owner_channel['channel_id'],
-    }
-    requests.post(
-        f"{config.url}channel/join/v2",
-        json=data_join
-    )
-    data_removeowner = {
-        'token': user3['token'],
-        'channel_id': two_owner_channel['channel_id'],
-        'u_id': two_owner_channel['owner1']['auth_user_id'],
-    }
-    response_removeowner = requests.post(
-        f"{config.url}channel/removeowner/v1",
-        json=data_removeowner
-    )
-    assert response_removeowner.status_code == 403
+def test_no_owner_permissions_removeowner(two_owner_channel, user3):
+    channel_join(user3['token'], two_owner_channel['channel_id'])
+    with pytest.raises(AccessError):
+        channel_removeowner(
+            user3['token'],
+            two_owner_channel['channel_id'],
+            two_owner_channel['owner1']['auth_user_id']
+        )
