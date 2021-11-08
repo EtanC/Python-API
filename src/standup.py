3f20bf2fe1700ff2e4530a4e76d93bfce515db9c
Braian <z5358768@ad.unsw.edu.au>
2021-11-08 17:15:29 +1100

    Modified standup so that when sending standup message it doesnt check for valid token, fixed timing issue with tests

--- src/standup.py
+++ src/standup.py
@@ -6,8 +6,10 @@ import threading
 import time
 from datetime import datetime, timezone
 
-def start_standup(token, channel_id, length):
+def start_standup(user, channel_id, length):
+    # Wait for messages from standup to be sent
     time.sleep(length)
+    # Collect all messages from standup
     standup_message = ""
     store = data_store.get()
     channel = get_channel(channel_id, store)
@@ -15,11 +17,21 @@ def start_standup(token, channel_id, length):
         standup_message += (
             f"{message['user']['handle_str']}: {message['message']}\n"
         )
-    message_send_v1(token, channel_id, standup_message)
+    # User who initiated standup sends the message
+    timestamp = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+    message = {
+        'message_id' : store['message_id'],
+        'u_id' : user['u_id'],
+        'message' : standup_message,
+        'time_sent' : timestamp,
+        'reacts' : [],
+        'is_pinned' : False,
+    }
+    channel['messages'].insert(0, message)
+    # Clean up standup since standup ended
     del channel['standup']
     data_store.set(store)
     return
-
 
 def standup_start_v1(token, channel_id, length):
     store = data_store.get()
@@ -33,12 +45,14 @@ def standup_start_v1(token, channel_id, length):
         raise InputError(
             description="Invalid standup duration, must be positive integer"
         )
+    # Assuming that every standup that finishes will remove key 'standup',
+    # if key doesnt exist, no standup currently active
     if 'standup' in channel:
         raise InputError(description="Active standup already running in channel")
     if not user in channel['all_members']:
         raise AccessError(description="User not a member of the channel")
     standup_thread = threading.Thread(
-        target=start_standup, args=(token, channel_id, store, channel, length)
+        target=start_standup, args=(user, channel_id, length)
     )
     channel['standup'] = {
         'initiator': user,
@@ -46,4 +60,5 @@ def standup_start_v1(token, channel_id, length):
     }
     data_store.set(store)
     standup_thread.start()
-    return {}
+    current_time = datetime.now().replace(tzinfo=timezone.utc).timestamp()
+    return {'time_finish' : current_time + length}
