ab7abd741ba0fdf723d3dbddc382678e0cf20bf5
cs1531 <cs1531@cse.unsw.edu.au>
2021-11-16 12:16:06 +1100

    Results push

--- fake/other.py
+++
@@ -1,9 +0,0 @@
-import requests
-from src import config
-from fake.util import parse_response
-
-def clear():
-    response = requests.delete(
-        f'{config.url}clear/v1',
-    )
-    return parse_response(response)
